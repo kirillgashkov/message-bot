96be138e36f6d255e90c6ca92ecc4057ae337280
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-02-01 02:35:07 +0500

    Finish database module's core

--- message_bot/database/__init__.py
+++ message_bot/database/__init__.py
@@ -2,35 +2,4 @@
 Provides API for interacting with database.
 """
 
-from typing import Dict
-import datetime
-
-
-#
-# Reading
-#
-
-
-def student_tags(date: datetime.date) -> Dict[int, str]:
-    pass
-
-
-def student_name(student: int) -> str:
-    pass
-
-
-def student_id(student: int) -> str:
-    pass
-
-
-#
-# Writing
-#
-
-
-def tag(date: datetime.date, student: int, value: str):
-    pass
-
-
-def untag(date: datetime.date, student: int):
-    pass
+from . import engines
---
+++ message_bot/database/engines/__init__.py
@@ -0,0 +1,4 @@
+
+from .base import BaseEngine
+from .gspread import GspreadEngine
+from .json import JSONEngine
