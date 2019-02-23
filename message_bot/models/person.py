364630599892de06c3f05f10cc7db3be1817c9cd
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-02-23 18:40:32 +0500

    Add stranger tag

--- message_bot/models/person.py
+++ message_bot/models/person.py
@@ -3,11 +3,19 @@ An object that holds person's ID, tags and optionally fields.
 """
 
 import dataclasses
-from typing import List, Dict
+from typing import List, Dict, Any
 
 
 @dataclasses.dataclass
 class Person:
     id: str
     tags: List[str]
-    fields: Dict[str, str]
+    fields: Dict[str, Any]
+
+
+#
+# Tags
+#
+
+
+TAG_STRANGER = 'STRANGER'
