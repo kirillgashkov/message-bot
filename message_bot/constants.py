7a804205a86baa5433830fb3db74466823dd73f4
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-02-23 18:10:37 +0500

    Remove constants related to other modules

--- message_bot/constants.py
+++ message_bot/constants.py
@@ -1,23 +1,16 @@
 """
-Constants
+Contains constants shared between other models.
 """
 
 import os
-import re
-from foodbot.commands import help_command
 
 
 #
-# Commands
+# General
 #
 
 
-DOMAIN_NAME = 'food'
 DATE_FORMAT = '%d/%m/%Y'
-STUDENT_REGEX = re.compile(r'^(0?[1-9]|1[0-9]|2[0-5])$')
-HELP_OFFER_ON_ERROR = (
-    f'Если вам нужна помощь, отправьте "{DOMAIN_NAME} {help_command.KEYWORD}"'
-)
 
 
 #
@@ -28,11 +21,3 @@ HELP_OFFER_ON_ERROR = (
 DIR_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 GSPREAD_CREDS = os.path.join(DIR_ROOT, 'credentials', 'gspread.json')
 VK_API_CREDS = os.path.join(DIR_ROOT, 'credentials', 'vk_api.json')
-
-
-#
-# Tags
-#
-
-
-TAG_STRANGER = 'STRANGER'
