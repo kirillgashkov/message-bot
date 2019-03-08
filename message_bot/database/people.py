80d4ad501c208798ef76e558088c550a376f7132
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-03-09 03:47:34 +0500

    Apply base engine changes to database.person

--- message_bot/database/people.py
+++ message_bot/database/people.py
@@ -3,7 +3,7 @@ Provides API for accessing database with people.
 """
 
 from message_bot import models, database
-from message_bot.constants import TAG_STRANGER
+from message_bot.models.person import TAG_STRANGER
 
 
 #
@@ -27,13 +27,17 @@ def set_engine(new_engine: database.engines.BaseEngine):
 _people = dict()
 
 
-def person(identifier: str) -> models.Person:
-    if identifier not in _people:
-        fields = engine.read(identifier)
-        if fields is None:
-            new_person = models.Person(identifier, [TAG_STRANGER], dict())
-        else:
-            tags = fields.pop('tags')
-            new_person = models.Person(identifier, tags.split(','), fields)
-        _people[identifier] = new_person
-    return _people[identifier]
+def get_person(identifier: str) -> models.Person:
+    if identifier in _people:
+        return _people[identifier]
+
+    raw_fields = engine.read(identifier)
+    if raw_fields is None:
+        new_person = models.Person(identifier, [TAG_STRANGER], dict())
+    else:
+        fields = dict(raw_fields)
+        tags = fields.pop('tags')
+        new_person = models.Person(identifier, tags.split(','), fields)
+
+    _people[identifier] = new_person
+    return new_person
