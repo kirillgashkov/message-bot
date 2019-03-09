abe4659143efeff8b1e5bfea1f2c879cc81a800a
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-03-09 15:41:51 +0500

    Add HELP_OFFER_ON_ERROR constant

--- message_bot/constants.py
+++ message_bot/constants.py
@@ -4,6 +4,8 @@ Contains constants shared between other modules.
 
 import os
 
+from message_bot import commands
+
 
 #
 # General
@@ -21,3 +23,14 @@ DATE_FORMAT = '%d/%m/%Y'
 DIR_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 GSPREAD_CREDS = os.path.join(DIR_ROOT, 'credentials', 'gspread.json')
 VK_API_CREDS = os.path.join(DIR_ROOT, 'credentials', 'vk_api.json')
+
+
+#
+# Strings
+#
+
+
+HELP_OFFER_ON_ERROR = (
+    f'Если вам нужна помощь, отправьте "{commands.foodbot.SUPER_COMMAND} '
+    f'{commands.foodbot.ShowHelpCommand.keyword}"'
+)
